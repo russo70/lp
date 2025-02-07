9d6f1ff18cf980512a75a4ddb94b07700e8605ad
russo70 <164194506+russo70@users.noreply.github.com>
2025-02-07 11:51:45 +0100

    add

--- progetto lp febbreio/huffman-codes.pl
+++ progetto lp febbreio/huffman-codes.pl
@@ -5,9 +5,35 @@
 :- dynamic hucodec_generate_symbol_bits_table/2.
 :- dynamic hucode_print_huffman_tree/1.
 
+void_list([]):-!.
+
+one_element_list(List):-
+    List=(_|L),
+    void_list(L),
+    !.
+
+
 
 %hucodec_generate_huffman_tree
 
 hucodec_generate_huffman_tree(List, Tree):-
+    not(void_list(List)),
+    not(one_element_list(List)),
+    Tree=0,
+    !.
 
+
+create_new_node(A, B, C):-
+    A=(L, N),
+    number(N),
+    not(number(L)),
+    B=(L1,N1);
+    number(N1),
+    not(number(L1)),
+    C=((L,L1),N + N1),
+    writeln(C),nl,
+    !.
+
+head_node(List):-
+    List=(),
     !.
