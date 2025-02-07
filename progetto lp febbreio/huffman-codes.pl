e1e2b874192ce1e59c8c82c13fa448bcbeac83af
russo70 <164194506+russo70@users.noreply.github.com>
2025-02-07 11:09:34 +0100

    ass

---
+++ progetto lp febbreio/huffman-codes.pl
@@ -0,0 +1,13 @@
+:- dynamic hucodec_decode/3.
+:- dynamic hucodec_encode/3.
+:- dynamic hucodec_encode_file/3.
+:- dynamic hucodec_generate_huffman_tree/2.
+:- dynamic hucodec_generate_symbol_bits_table/2.
+:- dynamic hucode_print_huffman_tree/1.
+
+
+%hucodec_generate_huffman_tree
+
+hucodec_generate_huffman_tree(List, Tree):-
+
+    !.
