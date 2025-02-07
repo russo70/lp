efa69172b8d6ded522bde92fe1808c83fd3c44ee
russo70 <164194506+russo70@users.noreply.github.com>
2025-02-07 16:09:09 +0100

    add

--- progetto lp febbreio/huffman-codes.pl
+++ progetto lp febbreio/huffman-codes.pl
@@ -1,4 +1,4 @@
-:- dynamic hucodec_decode/3.
+:- dynamic node/2.
 :- dynamic hucodec_encode/3.
 :- dynamic hucodec_encode_file/3.
 :- dynamic hucodec_generate_huffman_tree/2.
@@ -19,33 +19,37 @@ hucodec_generate_huffman_tree(List, Tree):-
     not(one_element_list(List)),
     head_node(List,C):
     Tree=C,
-    writeln(Tree),
+    writeln(C),
     !.
 
 
-create_new_node(A, B, C):-
-    A=(L,N),
+create_new_node([_|N]):-
     number(N),
-    not(number(L)),
-    B=(L1,N1);
-    number(N1),
-    not(number(L1)),
-    C=((L,L1),N + N1),
-    writeln(C),nl,
+    %not(number(L)),
     !.
+
+
+ %   prova(B, N, L):-
+ %   B=[L1|N1];
+ %   number(N1),
+ %   not(number(L1)),
+ %   C=[[L,L1],N+N1],
+ %   writeln(C),
+
+ %   !.
 
 head_node(List,C):-
-    List1=(A|List),
+    List1=(_|List),
     not(one_element_list(List1)),
     head_node(List1,C),
-    List1=(B|_),
-    create_new_node(A,B,C),
+   % List1=(B|_),
+   % create_new_node(A,B,C),
     !.
 
-head_node(List,C):-
-    List=(A|List1),
+head_node(List):-
+    List=(_|List1),
     one_element_list(List1),
-    create_new_node(A,List1,C),
-    List=(A|C|List1),
+ %   create_new_node(A,List1,C),
+ %   List=(A|C|List1),
     !.
 
